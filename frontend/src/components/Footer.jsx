996a0c62fba027f94edb27f73f51a4734025dc49
sushma <2200031124cseh@gmail.com>
2025-02-20 23:11:42 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
+			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
+				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
+					© {new Date().getFullYear()} All rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
